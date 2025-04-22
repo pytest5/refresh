95ae3a854ad9d64c8cbc7aec824dd6f77ce7f620
pytest5 <pytest514@gmail.com>
2025-04-22 11:06:38 +0800

    fix: update initial state type for usestate

--- src/components/TodoApp/index.tsx
+++ src/components/TodoApp/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Form } from "./Form/Form";
 import { Todos } from "./Todos/Todos";
+import { ToDo } from "./utils/types";
 
 export const TodoApp = () => {
-  const [todos, setTodos] = React.useState<Array<string>>([]);
+  const [todos, setTodos] = React.useState<Array<ToDo>>([]);
   return (
     <>
       <div>Hi im a todo list</div>
       <Form setTodos={setTodos} todos={todos} />
-      <Todos todos={todos} />
+      <Todos setTodos={setTodos} todos={todos} />
     </>
   );
 };
