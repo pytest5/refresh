26d47f93f7b0f2eb2a63a9bcf325eee3e5c0a8b6
pytest5 <pytest514@gmail.com>
2025-04-22 10:41:49 +0800

    feat: refactor types

--- src/components/TodoApp/Todos/Todos.tsx
+++ src/components/TodoApp/Todos/Todos.tsx
@@ -1,8 +1,15 @@
-export const Todos = ({ todos }: { todos: Array<string> }) => {
+import React from "react";
+import { DeleteButton } from "../DeleteButton/DeleteButton";
+import { TodoStateProps } from "../utils/types";
+
+export const Todos = ({ todos, setTodos }: TodoStateProps) => {
+  const deleteTodo = (id: number) => todos.filter(({ id }) => id === id);
   return (
     <ul>
       {todos.map((i) => (
-        <li>{i}</li>
+        <li>
+          {i} <DeleteButton />
+        </li>
       ))}
     </ul>
   );
